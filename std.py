973be4e4da8c776f7f973089639c6b8801956966
deeksha487 <deekshavernekar11@gmail.com>
2025-11-04 11:40:37 +0530

    Create std.py

---
+++ std.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) ==3:
+script name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+print("user provided input values:")
+else:
+scrpit name = sys.argv[0]
+name = "Deeksha"
+rollno = "101"
+print("No input given - using default values:")
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
+
